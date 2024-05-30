882de86b3f68af19c9b440f39ae6040f5b37c55f
Popole <falqleopaul@gmail.com>
2024-05-30 13:57:55 +0200

    enregistre en db

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from flask import Flask, render_template
 import serial
 import threading
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -9,18 +11,54 @@ socketio = SocketIO(app)
 port = 'COM5'
 baud_rate = 115200
 
+# Configuration de MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['Chute']
+collection = db['falling']
+
 
 def read_from_port(ser):
     while True:
         if ser.in_waiting:
             line = ser.readline().decode('utf-8').rstrip()
             print(line)
-            socketio.emit('new_message', {'message': line})
+
+            try:
+                msg_type, card_id = line.split(';')
+
+                if msg_type.lower() == 'chute':
+                    # Insertion dans MongoDB
+                    date = datetime.now()
+                    data = {'date': date, 'card_id': card_id}
+                    collection.insert_one(data)
+                    print(f"Inséré dans MongoDB: {data}")
+
+                    # Récupérer les 20 derniers messages triés par date décroissante
+                    last_messages = collection.find().sort('date', -1).limit(20)
+                    messages = [{'date': msg['date'].strftime('%Y-%m-%d %H:%M:%S'), 'card_id': msg['card_id']} for msg in last_messages]
+                    # Émettre les messages au client connecté
+                    socketio.emit('last_messages', messages)
+
+            except ValueError:
+                print("Erreur de format du message")
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@socketio.on('connect')
+def handle_connect():
+    # Récupérer les 20 derniers messages triés par date décroissante
+    last_messages = collection.find().sort('date', -1).limit(20)
+    messages = [{'date': msg['date'], 'card_id': msg['card_id']} for msg in last_messages]
+    # Convertir les dates en chaînes de caractères
+    for message in messages:
+        message['date'] = message['date'].strftime('%Y-%m-%d %H:%M:%S') if isinstance(message['date'], datetime) else \
+        message['date']
+    # Émettre les messages au client connecté
+    emit('last_messages', messages)
 
 
 if __name__ == '__main__':
